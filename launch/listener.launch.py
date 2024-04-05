a535c2244c50ed64d24d40a2d21c21a718520af0
TobsBP <tobias.bueno2005@gmail.com>
2024-04-05 16:52:33 -0300

    Primeiro commit

---
+++ launch/listener.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def gerenrate_launch_description():
+  return LaunchDescription([
+    Node(
+      package='demo_nodes_cpp',
+      executable='listener'
+    )
+  ])
