f91606fe500b9b9b852365f9b2841cc01afce733
TobsBP <tobias.bueno2005@gmail.com>
2024-04-05 17:03:27 -0300

    Segundo commit

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def gerenrate_launch_description():
   return LaunchDescription([
     Node(
-      package='demo_nodes_cpp',
+      package='demo_nodes_py',
       executable='listener'
     )
   ])
